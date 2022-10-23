5ebe9b258a9e839d4a41ecd41f506f9483a4b8e3
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-10-22 20:23:01 -0400

    api key added

---
+++ script.js
@@ -0,0 +1,17 @@
+function getData() {
+    // let loc 
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=Kabul&appid=2405d338b8f6702827e8a40da43d496c`;
+    const xhr = new XMLHttpRequest()
+    xhr.open("GET", url, true)
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let data = this.responseText
+            data = JSON.parse(data)
+            console.log(data);
+        } else {
+            alert("some error occured")
+        }
+    }
+    xhr.send()
+}
+getData()
