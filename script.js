7f7c228c7fb75590ea4a2d9b9581cfb4cf5f797f
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-10-24 00:31:35 -0400

    adding data content to script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const cityName = document.getElementById("cityName");
+const country = document.getElementById("country");
+const timeZone = document.getElementById("timeZone");
+const coords = document.querySelector(".coord").querySelectorAll("p");
+const weatherType = document.getElementById("weatherType");
+const weatherDesc = document.getElementById("weatherDesc");
+const temp = document.querySelector(".temp").querySelectorAll("p");
+const visibility = document.getElementById("visibility");
+const wind = document.querySelector(".wind").querySelectorAll("p");
+const clouds = document.getElementById("clouds");
+const weatherImg = document.getElementById("weatherImg");
+
+
+
 function getData() {
     // let loc 
     let url = `http://api.openweathermap.org/data/2.5/weather?q=Virginia&appid=2405d338b8f6702827e8a40da43d496c`;
