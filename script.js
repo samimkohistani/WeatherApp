19bc7dd8ad983459e0c08922bec745bee5a1d849
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-10-24 00:39:32 -0400

    adding the api data to javascript

--- script.js
+++ script.js
@@ -19,9 +19,28 @@ function getData() {
     xhr.open("GET", url, true)
     xhr.onload = function () {
         if (this.status === 200) {
-            let data = this.responseText
-            data = JSON.parse(data)
-            
+
+            let data = JSON.parse(this.responseText);
+            cityName.textContent = `Location : ${data.name}`;
+            country.textContent = `Country : ${data.sys.country}`;
+            timeZone.textContent = `Timezone : ${data.timezone}`;
+            coords[0].textContent = `Longitude : ${data.coord.lon}`;
+            coords[1].textContent = `Latitude : ${data.coord.lat}`;
+            weatherType.textContent = `Weather type : ${data.weather[0].main}`;
+            weatherDesc.textContent = `Weather description : ${data.weather[0].description}`;
+            weatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            temp[0].textContent = `Original Temperature : ${ktc(data.main.temp)}`;
+            temp[1].textContent = `But it feels like ${ktc(data.main.feels_like)}`;
+            temp[2].textContent = `Min temperature ${ktc(data.main.temp_min)}`;
+            temp[3].textContent = `Max temperature ${ktc(data.main.temp_max)}`;
+            temp[4].textContent = `Pressure : ${data.main.pressure}`;
+            temp[5].textContent = `Humidity : ${data.main.humidity}`;
+            visibility.textContent = `Visibility : ${data.visibility}`;
+            wind[0].textContent = `Wind speed : ${data.wind.speed}`;
+            wind[1].textContent = `Wind deg : ${data.wind.deg}`;
+
+
+
         } else {
             alert("some error occured")
         }
