82fdaa719b2b22da1535a97bb5b8fdb4edd32880
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-10-24 00:08:37 -0400

    adding content to html

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function getData() {
     // let loc 
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=Kabul&appid=2405d338b8f6702827e8a40da43d496c`;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=Virginia&appid=2405d338b8f6702827e8a40da43d496c`;
     const xhr = new XMLHttpRequest()
     xhr.open("GET", url, true)
     xhr.onload = function () {
         if (this.status === 200) {
             let data = this.responseText
             data = JSON.parse(data)
-            console.log(data);
+            
         } else {
             alert("some error occured")
         }
